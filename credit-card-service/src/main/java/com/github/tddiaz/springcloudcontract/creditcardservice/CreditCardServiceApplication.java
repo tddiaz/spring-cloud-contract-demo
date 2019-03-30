d4bb0d7fbe6685be377cb4b5a6879b1765d3a724
Tristan Diaz <tristantomdiaz@gmail.com>
2019-03-30 21:08:21 +0400

    initial commit

---
+++ credit-card-service/src/main/java/com/github/tddiaz/springcloudcontract/creditcardservice/CreditCardServiceApplication.java
@@ -0,0 +1,13 @@
+package com.github.tddiaz.springcloudcontract.creditcardservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreditCardServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreditCardServiceApplication.class, args);
+	}
+
+}
